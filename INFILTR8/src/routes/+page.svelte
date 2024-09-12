89c8171aa49dae5dacc37ea157835896be1e7ce6
cpedroza01 <cspedroza@miners.utep.edu>
2024-09-12 01:26:11 -0600

    analysis page

--- INFILTR8/src/routes/+page.svelte
+++ INFILTR8/src/routes/+page.svelte
@@ -33,4 +33,5 @@
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
     <a href="/report" class="button">Go to Report</a>
+    <a href="/analysis" class="button">Testing page</a>
 </div>
