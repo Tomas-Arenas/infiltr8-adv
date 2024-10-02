a9e3f633c17f0bb6ce5fbf0930ac82b5e6b8082f
Adrian Bonilla <adrian.bonilla1993@gmail.com>
2024-10-02 00:55:21 -0600

    Add notification functionality to layout component

--- INFILTR8/src/routes/+layout.svelte
+++ INFILTR8/src/routes/+layout.svelte
@@ -1,14 +1,30 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Darkmode from '$lib/components/Darkmode.svelte';
 	import { menuOpen } from '$lib/stores.js';
+	import { addNewNotification } from '$lib/stores/notifications.js';
+	import Toast from '$lib/components/Toast.svelte';
 
+	const mockNotifications = [
+        { id: 1, message: 'Mock Notification 1', type: 'message', timestamp: '2024-01-01T12:00:00', new: true },
+        { id: 2, message: 'Mock Notification 2', type: 'success', timestamp: '2024-01-02T13:00:00', new: true },
+        { id: 3, message: 'Mock Notification 3', type: 'error', timestamp: '2024-01-03T14:00:00', new: false }
+    ];
+
+	onMount(() => {
+		mockNotifications.forEach(notification => {
+			addNewNotification(notification);
+		});
+	});
 </script>
 
 <svelte:head>
 	<title>INFILTR8</title>
 </svelte:head>
+
+<Toast />
 
 <div class="fixed right-0 z-50 pr-3 pt-2">
 	<Darkmode />
@@ -16,7 +32,7 @@
 
 <div class="app flex h-screen bg-gray-50 dark:bg-gray-600">
 	<Navbar />
-	<div class={`p-4 flex-1 bg-gray-50 dark:bg-gray-600 ${$menuOpen ? 'ml-64' : ''} mt-10`}>
+	<div class={`flex-1 bg-gray-50 p-4 dark:bg-gray-600 ${$menuOpen ? 'ml-64' : ''} mt-10`}>
 		<main class="">
 			<slot />
 		</main>
@@ -29,5 +45,4 @@
 		flex-direction: column;
 		min-height: 100vh;
 	}
-
 </style>
