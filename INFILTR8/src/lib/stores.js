c8ed5ca07144d5fb53d1b323124944de0814ed46
Tomas-Arenas <tomas.arenaz25@gmail.com>
2024-10-02 12:45:22 -0600

    refactoring and fixing list functionality

--- INFILTR8/src/lib/stores.js
+++ INFILTR8/src/lib/stores.js
@@ -2,3 +2,9 @@ import { writable } from "svelte/store";
 
 //Menu Stuff
 export const menuOpen = writable(true);
+
+//scope stuff(info store in project route)
+export const ipsAllowed = writable([]);
+export const ipsDisallowed = writable([]);
+
+
