16a500e0c5caa7c0703cceb90b3942a0adf106a0
ErikLaN68 <eriklaneave@gmail.com>
2024-10-10 02:00:30 -0600

    added the path to upload a file and made sure it worked. Working on getting the rest of the parser set up

--- INFILTR8/backend/classes/database.py
+++ INFILTR8/backend/classes/database.py
@@ -1,19 +1,16 @@
 from neo4j import GraphDatabase
-from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-
-URI = config['URI']
-AUTH = (config['USERNAME'], config['PASSWORD'])
-
-driver = GraphDatabase.driver(URI, auth=AUTH)
-
-records, summary, keys = driver.execute_query(
-    "MATCH (n:Person) RETURN n LIMIT 25",
-    database_="neo4j",
-)
-
-for record in records:
-    # print(record)
-    # print(type(record))
-    print(record[0]['name'])
+class DataBase:
+    
+    def __init__(self,URI, AUTH):
+        self.URI = URI
+        self.AUTH = AUTH
+    
+    def driver_make(self):
+        return GraphDatabase.driver(self.URI, auth=self.AUTH)
+    
+    def run_query(self, query):
+        driver = self.driver_make()
+        records, summary, keys = driver.execute_query(query, database_="neo4j")
+        driver.close()
+        return records
