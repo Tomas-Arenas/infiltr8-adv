2e4b7dca300b41182446b28466cfa7e4afe7d9ff
Adrian Bonilla <adrian.bonilla1993@gmail.com>
2024-10-11 14:36:47 -0600

    Add notifications component to layout

--- INFILTR8/src/routes/+layout.svelte
+++ INFILTR8/src/routes/+layout.svelte
@@ -6,46 +6,73 @@
 	import { menuOpen } from '$lib/stores.js';
 	import { addNewNotification } from '$lib/stores/notifications.js';
 	import Toast from '$lib/components/Toast.svelte';
-  
+	import Notifications from '$lib/components/Notifications.svelte';
+
 	const mockNotifications = [
-		{ id: 1, message: 'Mock Notification 1', type: 'message', timestamp: '2024-01-01T12:00:00', new: true, timeout: 3000, isPermanent: false }, // Temporary
-		{ id: 2, message: 'Mock Notification 2', type: 'success', timestamp: '2024-01-02T13:00:00', new: true, timeout: 3000, isPermanent: true }, // Permanent
-		{ id: 3, message: 'Mock Notification 3', type: 'error', timestamp: '2024-01-03T14:00:00', new: false, timeout: 3000, isPermanent: true } // Permanent
+		{
+			id: 1,
+			message: 'Mock Notification 1',
+			type: 'message',
+			timestamp: '2024-01-01T12:00:00',
+			new: true,
+			timeout: 3000,
+			isPermanent: false
+		}, // Temporary
+		{
+			id: 2,
+			message: 'Mock Notification 2',
+			type: 'success',
+			timestamp: '2024-01-02T13:00:00',
+			new: true,
+			timeout: 3000,
+			isPermanent: true
+		}, // Permanent
+		{
+			id: 3,
+			message: 'Mock Notification 3',
+			type: 'error',
+			timestamp: '2024-01-03T14:00:00',
+			new: false,
+			timeout: 3000,
+			isPermanent: true
+		} // Permanent
 	];
-  
+
 	onMount(() => {
 		console.log('Loading mock notifications...');
-		mockNotifications.forEach(notification => {
+		mockNotifications.forEach((notification) => {
 			addNewNotification(notification); // Add each notification (temporary or permanent)
 		});
 		console.log('Mock notifications loaded.');
 	});
-  </script>
-  
-  <svelte:head>
+</script>
+
+<svelte:head>
 	<title>INFILTR8</title>
-  </svelte:head>
-  
-  <Toast />
-  
-  <div class="fixed right-0 z-50 pr-3 pt-2">
+</svelte:head>
+
+<Toast />
+
+<div class="fixed right-0 z-50 flex justify-items-center items-center gap-3 pr-3 pt-2">
+	<!-- Notifications Bell -->
+	<Notifications />
+	<!-- Darkmode Toggle -->
 	<Darkmode />
-  </div>
-  
-  <div class="app flex h-screen bg-gray-50 dark:bg-gray-600">
+</div>
+
+<div class="app flex h-screen bg-gray-50 dark:bg-gray-600">
 	<Navbar />
 	<div class={`flex-1 bg-gray-50 p-4 dark:bg-gray-600 ${$menuOpen ? 'ml-64' : ''} mt-10`}>
-	  <main class="">
-		<slot />
-	  </main>
+		<main class="">
+			<slot />
+		</main>
 	</div>
-  </div>
-  
-  <style>
+</div>
+
+<style>
 	.app {
-	  display: flex;
-	  flex-direction: column;
-	  min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
 	}
-  </style>
-  
+</style>
