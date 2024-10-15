ed034cf70f419f182cc33b29dfbd40b9f7352bf0
denisseloyizz <denisselv12345@gmail.com>
2024-10-14 20:25:10 -0600

    darkmode

---
+++ INFILTR8/src/lib/components/stores.js
@@ -0,0 +1,8 @@
+import { writable } from "svelte/store";
+
+//Menu Stuff
+export const menuOpen = writable(false);
+
+//scope stuff(info store in project route)
+export const ipsAllowed = writable([]);
+export const ipsDisallowed = writable([]);
