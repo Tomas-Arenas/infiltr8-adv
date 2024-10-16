e07829017a99fb7719b35981ec1c6f7118cbe384
ErikLaN68 <eriklaneave@gmail.com>
2024-10-16 17:13:53 -0600

    changed

--- starter.py
+++ starter.py
@@ -2,16 +2,25 @@ import platform
 import subprocess
 import os
 import threading
+import shutil
+
+python_path = shutil.which('python3') or shutil.which('python')
+npm_path = shutil.which('npm')
 
 # Make changes here as needed depending on if your machine is being annoying
-frontStart = ['npm', 'run', 'dev']
-backendStart = ['flask', 'run', '--debug']
+frontStart = [npm_path, 'run', 'dev']
+backendStart = [python_path, '-m', 'flask', 'run', '--debug']
+print(frontStart)
+print(backendStart)
 # Might need to change this to python3 for you windows people
-backendStartWindows = ['python','flask', 'run', '--debug']
+backendStartWindows = [python_path, '-m', 'flask', 'run', '--debug']
 
 def start_front():
     os.chdir('INFILTR8/')
     subprocess.run(frontStart)
+    # proc1 = subprocess.Popen(
+        
+    # )
 
 def start_back():
     os.chdir('backend/')
