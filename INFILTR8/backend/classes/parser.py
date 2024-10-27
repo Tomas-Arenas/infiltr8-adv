bdf5ac063605273a1fd2f3469164841437cdd5b1
ErikLaN68 <eriklaneave@gmail.com>
2024-10-27 17:45:36 -0600

    small changes to not have a hardcoded paths

--- INFILTR8/backend/classes/parser.py
+++ INFILTR8/backend/classes/parser.py
@@ -7,10 +7,10 @@ import category_encoders as ce
 from sklearn.preprocessing import MinMaxScaler
 
 # Path to the Nessus XML file, change line 10 to match path where your NESSUS file is
-nessus_file = '/Users/tomas/CS4311_INFILTR8_Team1_DecafCats_Fall2024/INFILTR8/backend/nessus-drop/Nessus_scan_file1.nessus'
+nessus_file = os.getcwd()+'/nessus-drop/nessus_v_unknown.nessus'
 
 # Base directory for output CSV files, change line 13 to where you want output CSVs to go
-output_base_dir = '/Users/tomas/CS4311_INFILTR8_Team1_DecafCats_Fall2024/INFILTR8/backend/output'
+output_base_dir = os.getcwd()+'/output/'
 
 # Construct paths for output CSV files
 data_with_exploits_path = os.path.join(output_base_dir, 'data_with_exploits.csv')
