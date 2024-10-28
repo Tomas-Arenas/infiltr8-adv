73a23a9b6075ff67513375fae6ae08574122770d
Adrian Bonilla <107004911+Bonilla-Adrian@users.noreply.github.com>
2024-10-27 22:40:09 -0600

    Remove unused code and update layout structure

--- INFILTR8/src/routes/+layout.svelte
+++ INFILTR8/src/routes/+layout.svelte
@@ -1,57 +1,18 @@
 <script>
 	import '../app.css';
-	import { onMount } from 'svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Darkmode from '$lib/components/Darkmode.svelte';
 	import Colorblind from '$lib/components/Colorblind.svelte';
 	import { menuOpen } from '$lib/stores.js';
-	import { addNewNotification } from '$lib/stores/notifications.js';
 	import Toast from '$lib/components/Toast.svelte';
 	import Notifications from '$lib/components/Notifications.svelte';
-
-	const mockNotifications = [
-		{
-			id: 1,
-			message: 'Mock Notification 1',
-			type: 'message',
-			timestamp: '2024-01-01T12:00:00',
-			new: true,
-			timeout: 3000,
-			isPermanent: false
-		}, // Temporary
-		{
-			id: 2,
-			message: 'Mock Notification 2',
-			type: 'success',
-			timestamp: '2024-01-02T13:00:00',
-			new: true,
-			timeout: 3000,
-			isPermanent: true
-		}, // Permanent
-		{
-			id: 3,
-			message: 'Mock Notification 3',
-			type: 'error',
-			timestamp: '2024-01-03T14:00:00',
-			new: false,
-			timeout: 3000,
-			isPermanent: true
-		} // Permanent
-	];
-
-	onMount(() => {
-		console.log('Loading mock notifications...');
-		mockNotifications.forEach((notification) => {
-			addNewNotification(notification); // Add each notification (temporary or permanent)
-		});
-		console.log('Mock notifications loaded.');
-	});
 </script>
 
 <svelte:head>
 	<title>INFILTR8</title>
 </svelte:head>
 
+<!-- Toast notifications for temporary alerts -->
 <Toast />
 
 <div class="fixed right-0 z-50 flex justify-items-center items-center gap-3 pr-3 pt-2">
@@ -66,7 +27,7 @@
 <div class="app flex h-screen bg-gray-50 dark:bg-gray-600">
 	<Navbar />
 	<div class={`flex-1 bg-gray-50 p-4 dark:bg-gray-600 ${$menuOpen ? 'ml-64' : ''} mt-10`}>
-		<main class="">
+		<main>
 			<slot />
 		</main>
 	</div>
