90ccc611b6fdf5a9cf17c44739157416400c9a53
Sebastian Mercado <smercado4@miners.utep.edu>
2024-11-12 23:43:01 -0700

    Main added The analysis filter for result to be scanr into

--- INFILTR8/src/routes/analysis/+page.svelte
+++ INFILTR8/src/routes/analysis/+page.svelte
@@ -243,7 +243,26 @@
         <div id="middle" class="flex-top float-left  w-8/12  py-0">
             <!-- start of Summary-->
             <div id="bottomSettings" class="py-8 shadow-2xl dark:bg-gray-900">
-                <h2 class="text-center font-bold text-xl leading-none text-gray-900 dark:text-white me-1 dark:bg-gray-900" >Summary</h2>
+                <h2 class="flex justify-center items-center text-center font-bold text-xl leading-none text-gray-900 dark:text-white me-1 dark:bg-gray-900">
+                    <span class="flex-1 text-center">Summary</span>
+                    <button Dropdown button ChevronDownOutline class="p-0 bg-transparent border-none cursor-pointer outline-none ml-auto" aria-label="Filter Icon">
+                      <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
+
+                      </svg>
+                    </button>
+                    <Dropdown>
+                        <DropdownItem>Low Vulnerability</DropdownItem>
+                        <DropdownItem>Mid Vulnerability</DropdownItem>
+                        <DropdownItem>High Vulnerability</DropdownItem>
+                        <DropdownItem></DropdownItem>
+                      </Dropdown>
+                      
+                  </h2>
+                  
+                  
+                
+                  
                 <div class="overflow-y-auto h-11  max-w-2xs lg:h-[calc(100vh-30rem)] lg:block dark:bg-gray-800 lg:me-0 lg:sticky top-2 px-2 dark:text-white">
                     <p>Current Testing</p>
                     <p>Weak password Attack</p>
@@ -286,6 +305,7 @@
 
 
                     <p> {timestamp}</p>
+                    
                 </div>
             </div>
         </div>
