123c695924054cd67e1777904d8b840ea0438b2a
Adrian Bonilla <107004911+Bonilla-Adrian@users.noreply.github.com>
2024-11-13 00:36:06 -0700

    Add authentication protection to routes

--- INFILTR8/src/lib/stores/session.js
+++ INFILTR8/src/lib/stores/session.js
@@ -1,11 +1,6 @@
 // src/stores/session.js
 import { writable } from 'svelte/store';
 
-// $: $session = session.subscribe(($session) => {
-//     loggedIn = $session.logged_in;
-//     userInitials = $session.logged_in ? $session.username[0].toUpperCase() : 'U';    ---> used to get the values from session.
-// });                                                                                      good for components that only mount once, onmount unusable here (i.e. navbar)
-
 export const session = writable({
     logged_in: false,
     username: '',
@@ -13,13 +8,16 @@ export const session = writable({
 });
 
 // Fetch session status from the server
-export async function checkSession() {
-    const response = await fetch('/flask-api/check_session',{method: 'GET',});
+export async function checkSession(fetch = window.fetch) {  // Default to global fetch if none provided
+    const response = await fetch('/flask-api/check_session', {
+        method: 'GET',
+        credentials: 'include'
+    });
     const result = await response.json();
     session.set({
         logged_in: result.logged_in,
         username: result.username || '',
         session_id: result.session_id
     });
-    console.log(result.status, result.username, result.session_id)
+    console.log(result.status, result.username, result.session_id);
 }
